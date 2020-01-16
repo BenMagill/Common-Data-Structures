2a52c3da5934b5bc50a940e7002692ec76b39ee9
Ben Magill <34661412+BenMagill@users.noreply.github.com>
2020-01-16 20:17:45 +0000

    Started linked list

---
+++ src/LinkedList.js
@@ -0,0 +1,33 @@
+class LinkedList {
+    constructor(){
+        this.list = [] // Dynamic size as the idea is it can be expanded infinitly
+        this.startPointer = 0 // Location of first item
+    }
+
+    insert(){}
+
+    addToEnd(){}
+
+    addToStart(){}
+
+    remove(){}
+
+    lastItemIndex(){
+        var currIndex = this.startPointer
+        var nextIndex = undefined
+        var found = false
+        while (found !== true){
+            
+        }
+        return currIndex
+
+        /**
+         * Access first location
+         * Does next index = 0?
+         * No go to it and repeat
+         * Yes return index at
+         */
+    }
+}
+
+exports.default = LinkedList
